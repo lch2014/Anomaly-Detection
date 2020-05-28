21f6f1cda5e89d84d7a72e3a3e3b20b2910a7ff2
lch2014 <luch14@126.com>
2020-05-28 20:53:55 +0800

    mlp

--- NN-based/simple_nn.py
+++ NN-based/simple_nn.py
@@ -58,8 +58,19 @@ class AutoEncoder(nn.Module):
     
     def forward(self, x):
         encode = self.encoder(x)
-        decode = self.decoder(x)
+        decode = self.decoder(encode)
         return encode, decode
+
+class MLPClassifier(nn.Module):
+    def __init__(self, num_features, num_labels):
+        self.classifier = nn.Sequential(nn.Linear(num_features, 32),
+                                        nn.Dropout(0.5),
+                                        nn.ReLU(True),
+                                        nn.Linear(32, num_labels))
+    
+    def forward(self, x):
+        x = self.classifier(x)
+        return x
 
 if __name__ == "__main__":
     file_path = "../../split_csv_new/"
@@ -67,6 +78,7 @@ if __name__ == "__main__":
     lr = 0.01
     wd = 1e-5
     epoches = 40
+    num_labels = 13
 
     LE, X_train, Y_train, X_test, Y_test = get_data(file_path)
     num_features = X_train.shape[-1]
@@ -79,19 +91,19 @@ if __name__ == "__main__":
     train_loader = Data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     print("Train data loaded!")
 
-    #test_dataset = Data.TensorDataset(torch.from_numpy(X_test), torch.from_numpy(Y_test))
-    #test_loader = Data.DataLoader(test_dataset, batch_size=64, shuffle=True)
-    #print("Test data loaded!")
+    test_dataset = Data.TensorDataset(torch.from_numpy(X_test), torch.from_numpy(Y_test))
+    test_loader = Data.DataLoader(test_dataset, batch_size=64, shuffle=True)
+    print("Test data loaded!")
 
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+'''
     AE = AutoEncoder(num_features)
     criterion = nn.MSELoss()
     optimizier = torch.optim.Adam(AE.parameters(), lr=lr, weight_decay=wd)
 
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
     AE = AE.to(device)
 
-    print("Start training.............................")
+    print("Start training autoencoder.............................")
 
     for epoch in range(epoches):
         if epoch in [epoches * 0.25, epoches * 0.5]:
@@ -108,3 +120,46 @@ if __name__ == "__main__":
             optimizier.step()
 
         print("epoch: {}, loss is {}".format((epoch+1), loss.data.float()))
+'''
+    MC = MLPClassifier(num_features, num_labels)
+    criterion2 = nn.CrossEntropyLoss()
+    optimizier2 = torch.optim.Adam(MC.parameters(), lr=0.001)
+
+    MC = MC.to(device)
+
+    print("Start training classifier............................ ")
+
+    train_loss = 0.0
+    train_corrects = 0
+    step = 0
+    epoch = 0
+    iter_data = iter(train_loader)
+    MC.train()
+
+    while epoch < epoches:
+        try:
+            train_batch, train_label = next(iter_data)
+        except StopIteration:
+            print('Epoch: {}\tLoss:{}\tAcc:{}'.format(epoch+1, train_loss/len(train_loader.dataset), train_corrects.double()/len(train_loader.dataset)))
+            step = 0
+            train_loss = 0.0
+            train_corrects = 0
+            iter_data = iter(train_loader)
+            train_batch, train_label = next(iter_data)
+            epoch += 1
+
+        train_batch = train_batch.to(device)
+        train_label = train_label.to(device)
+        output = MC(train_batch)
+        loss = criterion2(output, train_label.long())
+        _, preds = torch.max(output, 1)
+        train_corrects += torch.sum(preds == train_label)
+        optimizier2.zero_grad()
+        loss.backward()
+        train_loss += loss.data.cpu().numpy()
+        optimizier2.step()
+        if (step+1) % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]'.format(
+                    epoch+1, (step + 1)* len(train_batch), len(train_loader.dataset),
+                    100. * (step + 1) / len(train_loader)))
+        step += 1
